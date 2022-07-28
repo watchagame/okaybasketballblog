3b75f03e68661b6d5e0cfb9a269022a63a55e6e4
watchagame <evancspellman@gmail.com>
2022-07-28 11:15:04 -0700

    npm check

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -2,7 +2,7 @@ const config = require(`../../.ghost.json`).production;
 module.exports = {
     siteUrl:
         process.env.NODE_ENV === `production`
-            ? process.env.SITE_URL || config.siteUrl || `https://okaybasketball.herokuapp.com`
+            ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
             : `https://okaybasketball.herokuapp.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
